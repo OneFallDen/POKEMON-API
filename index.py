4aecc716a1bd307131f618e1683482680178cee0
OneFallDen <onefallden@gmail.com>
2022-10-30 18:30:01 +0300

    added get_by_id request

--- index.py
+++ index.py
@@ -2,11 +2,11 @@ from fastapi import FastAPI
 import psycopg2
 
 con = psycopg2.connect(
-    database="DB_name",
-    user="user",
-    password="password",
-    host="example.com",
-    port="8000"
+    database="DB_NAME",  # DB_NAME
+    user="USER_NAME",  # USER_NAME
+    password="PASSWORD",  # PASSWORD
+    host="example.com",  # example.com
+    port="PORT"  # PORT
 )
 
 app = FastAPI()
@@ -19,4 +19,12 @@ def home():
 
 @app.get('/id')
 def get_pokemon_by_id(id_pokemon: int):
-    return {'id': id_pokemon}
+    cur = con.cursor()
+    cur.execute(f"SELECT * FROM pokemon WHERE id = {id_pokemon}")
+    rows = cur.fetchall()
+    for row in rows:
+        name = row[1]
+        category = row[2]
+        image_url = row[3]
+    con.close()
+    return {'id': id_pokemon, 'name': name, 'category': category, 'image_url': image_url}
