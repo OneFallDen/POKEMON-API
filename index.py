259d701bb459a1849209ce727a0c91ddebf7e22f
OneFallDen <onefallden@gmail.com>
2022-10-30 19:49:40 +0300

    added pokemons type and weakness

--- index.py
+++ index.py
@@ -14,25 +14,33 @@ def home():
 def get_pokemon_by_id(id_pokemon: int):
     con = connection()
     cur = con.cursor()
-    cur.execute(f"SELECT * FROM pokemon WHERE id = {id_pokemon}")
+    cur.execute("SELECT pokemon.id, pokemon.name,  pokemon.category, pokemon.image_url, type.type, "
+                "weaknesses.weakness FROM pokemon JOIN "
+                f"type ON pokemon.id = id_pokemon  JOIN weaknesses ON pokemon.id = pokemon_id  WHERE {id_pokemon} = 1;")
     rows = cur.fetchall()
     for row in rows:
         name = row[1]
         category = row[2]
         image_url = row[3]
+        type = row[4]
+        weakness = row[5]
     con.close()
-    return {'id': id_pokemon, 'name': name, 'category': category, 'image_url': image_url}
+    return {'id': id_pokemon, 'name': name, 'category': category, 'image_url': image_url, 'type': type, 'weakness': weakness}
 
 
 @app.get('/name')
 def get_pokemon_by_name(name: str):
     con = connection()
     cur = con.cursor()
-    cur.execute(f"SELECT * FROM pokemon WHERE name = '{name}'")
+    cur.execute("SELECT pokemon.id, pokemon.name,  pokemon.category, pokemon.image_url, type.type, "
+                "weaknesses.weakness FROM pokemon JOIN "
+                f"type ON pokemon.id = id_pokemon  JOIN weaknesses ON pokemon.id = pokemon_id  WHERE name = '{name}';")
     rows = cur.fetchall()
     for row in rows:
         id_pokemon = row[0]
         category = row[2]
         image_url = row[3]
+        type = row[4]
+        weakness = row[5]
     con.close()
-    return {'id': id_pokemon, 'name': name, 'category': category, 'image_url': image_url}
+    return {'id': id_pokemon, 'name': name, 'category': category, 'image_url': image_url, 'type': type, 'weakness': weakness}
