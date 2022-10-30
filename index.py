8613c9761a28e4688697511082d48b117564d95b
OneFallDen <onefallden@gmail.com>
2022-10-30 18:13:11 +0300

    db connect

--- index.py
+++ index.py
@@ -1,4 +1,13 @@
 from fastapi import FastAPI
+import psycopg2
+
+con = psycopg2.connect(
+    database="DB_name",
+    user="user",
+    password="password",
+    host="example.com",
+    port="8000"
+)
 
 app = FastAPI()
 
@@ -6,3 +15,8 @@ app = FastAPI()
 @app.get('/')
 def home():
     return 'Hello! It is Pokemon API'
+
+
+@app.get('/id')
+def get_pokemon_by_id(id_pokemon: int):
+    return {'id': id_pokemon}
